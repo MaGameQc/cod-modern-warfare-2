7acf88cb7e9155d769c8319e77aa43910b08b3ca
Tommy Audet <magame92@hotmail.com>
2021-06-09 10:21:59 -0400

    ajout de javascript et flexibilite

--- index.js
+++ index.js
@@ -22,3 +22,55 @@ window.setInterval(() => {
     bodyEl.style.backgroundImage = `radial-gradient(ellipse at bottom left, rgba(238, 194, 0, ${alpha}) 0%, rgba(100,100,100,1) 40%)`;
 }, 1);
 }
+
+class Menu {
+    constructor(){
+        this.menuItems = document.querySelectorAll(".card");
+        this.backgroundImg = document.querySelector(".soldierCont__ghost");
+
+        this.imgSources = [
+            "images/ghost.png",
+            "images/bgSoldier.png",
+            "images/bgSoldier2.png",
+        ];
+
+        this.addHoverListeners(this.menuItems);
+    }
+    addHoverListeners(elements){
+        elements.forEach((element, index)=>{
+            element.addEventListener("mouseover", ()=>{
+                this.backgroundImg.src = this.imgSources[index];
+            });
+        });
+    }
+}
+
+class AudPlayer{
+    constructor(){
+        this.audio = new Audio();
+        this.audio.src = "sounds/mw2song.m4a";
+
+        this.startAudio();
+        this.replayAudio();
+    }
+    startAudio(){
+        document.querySelector("body").addEventListener("mouseover", ()=>{
+            this.audio.play();            
+        });
+    }
+    replayAudio(){
+        this.audio.addEventListener("timeupdate", ()=>{
+            if(this.audio.currentTime >= this.audio.duration){
+                console.log("replay");
+                this.audio.currentTime = 0;
+                audio.play();
+            }
+        });
+    }
+}
+
+// let audPlayer = new AudPlayer();
+
+
+
+let menu = new Menu();
